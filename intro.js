b4ca58138cb3b1688da0ddd10886ed1b92184474
DBrezgunova <55583431+DBrezgunova@users.noreply.github.com>
2019-09-27 16:10:29 +0600

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,2 +1,2 @@
-let name = ('Дарья'), Name = ('Брезгунова'), intro = ('Салют'), language = ('JS');
-alert (intro + "меня зовут" + name + Name + "я изучаю" + language);
+let name = ('Дарья'), Name = ('Брезгунова'), intro = ('Салют!'), language = ('JS');
+alert (intro + "Меня зовут" + name + Name + "я изучаю" + language);
